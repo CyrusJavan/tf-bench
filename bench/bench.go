e95cfedd0dfd9a5b6fce17f498914c26ef3dc9a8
Cyrus Javan <javan.cyrus@gmail.com>
2021-07-06 13:32:31 -0700

    version awareness

--- bench/bench.go
+++ bench/bench.go
@@ -31,6 +31,8 @@ const (
 	clearLine          = "\033[2K"
 )
 
+var tf15 = version.Must(version.NewVersion("v0.15"))
+
 type Config struct {
 	SkipControllerVersion bool
 	Iterations            int
@@ -155,21 +157,17 @@ func Benchmark(cfg *Config) (*Report, error) {
 	report := NewReport(cfg)
 	// Run refresh of the entire workspace to get the TotalTime
 	fmt.Print("All resources measurement:  ")
-	var done bool
-	go printSpinner(&done)
 	t, err := measureRefresh(".", defaultParallelism, cfg.Iterations, cfg.VarFile)
-	done = true
-	time.Sleep(120 * time.Millisecond)
 	if err != nil {
 		return nil, fmt.Errorf("could not measure refresh for workspace: %w", err)
 	}
+	fmt.Println()
 	report.TotalTime = t
 
 	// Benchmark each resource type individually
-
 	for r, count := range resourceTypes {
 		fmt.Printf("%s measurement:  ", r)
-		rr, err := resourceBenchmark(cfg, &Resource{Name: r, Count: count}, state)
+		rr, err := resourceBenchmark(cfg, &Resource{Name: r, Count: count}, state, report.TerraformVersion)
 		if err != nil {
 			fmt.Printf("During the individual resource benchmark for resourceType=%s the following error occured: %v", r, err)
 			continue
@@ -203,13 +201,13 @@ func printSpinner(done *bool) {
 	}
 }
 
-func resourceBenchmark(cfg *Config, resource *Resource, state []byte) (*ResourceReport, error) {
+func resourceBenchmark(cfg *Config, resource *Resource, state []byte, tfv *TerraformVersion) (*ResourceReport, error) {
 	dir := os.TempDir()
 	defer os.RemoveAll(dir)
 	// Copy over any tfvars or tfvars.json files
 	_, _ = runCommand("/bin/sh", "-c", fmt.Sprintf("cp -R *.tfvars *.tfvars.json %s", dir))
 	// Generate the modified TF file
-	modifiedTf, err := createModifiedTerraformConfiguration(resource, cfg.VarFile)
+	modifiedTf, err := createModifiedTerraformConfiguration(resource, cfg.VarFile, tfv)
 	if err != nil {
 		return nil, fmt.Errorf("creating modified tf file: %w", err)
 	}
@@ -436,7 +434,7 @@ func terraformState() (*TerraformState, []byte, error) {
 	return &tfstate, state, nil
 }
 
-func createModifiedTerraformConfiguration(resource *Resource, varFile string) ([]byte, error) {
+func createModifiedTerraformConfiguration(resource *Resource, varFile string, tfVersion *TerraformVersion) ([]byte, error) {
 	// We want to build a tf file that contains just these block types:
 	// variable
 	// provider
@@ -476,7 +474,11 @@ func createModifiedTerraformConfiguration(resource *Resource, varFile string) ([
 						}
 					}
 				}
-				if block.Type() == "provider" {
+				tfv, err := version.NewVersion(tfVersion.TerraformVersion)
+				if err != nil {
+					tfv = version.Must(version.NewVersion("v0.12"))
+				}
+				if block.Type() == "provider" && tfv.GreaterThanOrEqual(tf15) {
 					if labels := block.Labels(); len(labels) > 0 {
 						label := labels[0]
 						if label != strings.Split(resource.Name, "_")[0] {
